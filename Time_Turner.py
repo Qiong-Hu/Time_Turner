20e7657553bcc0d51843ec7c875cc8047f83f168
junesirius <wojiaoziyou@gmail.com>
2021-05-17 19:58:42 -0700

    version 20200914

--- Time_Turner.py
+++ Time_Turner.py
@@ -1,178 +1,217 @@
+# -*- coding: utf-8 -*-
+# Copyright: Qiong Hu (junesirius@ucla.edu)
 import matplotlib.pyplot as plt
+from matplotlib import get_backend
 import time
 import numpy as np
 import winsound
 
-# params for plot
-linewidth = 6
-gap = 0.17
-base_pos = [[1, 1], [2.5, 1], [4.5, 1], [6, 1]]
+class Clock():
+    def __init__(self):
+        # Params for plot
+        self.linewidth = 6
+        self.gap = 0.17
+        self.base_pos = [[1, 1], [2.5, 1], [4.5, 1], [6, 1]]
 
+        # Personalized time transform rules and exempt rules
+        self.rule_turning_point     = [[0, 0], [ 7, 0], [8, 0], [11, 0], [12,0], [12,30], [14,30], [16,30], [17,30], [19,0], [20, 0], [22,30], [24, 0]]
+        self.rule_ref_turning_point = [[5,30], [12,30], [2,30], [12,30], [9, 0], [ 3, 0], [ 3, 0], [18,30], [20,30], [22,0], [ 2,30], [ 5, 0]]
+        self.rule_speed             = [     1,     2.5,      1,     2.5,      3,       1,       1,       2,       1,      2,       1,       1]
+        self.rule_name              = ["睡眠",   "家务", "学习",  "用餐",  "睡眠",  "学习",  "学习",   "家务",  "用餐", "电影",   "娱乐",  "家务"]
 
-# map num 0-9 to index in digital display
-def num_to_lineindex(num):
-    index = []
-    if num == 0:
-        index = [0, 1, 2, 4, 5, 6]
-    elif num == 1:
-        index = [2, 5]
-    elif num == 2:
-        index = [0, 2, 3, 4, 6]
-    elif num == 3:
-        index = [0, 2, 3, 5, 6]
-    elif num == 4:
-        index = [1, 2, 3, 5]
-    elif num == 5:
-        index = [0, 1, 3, 5, 6]
-    elif num == 6:
-        index = [0, 1, 3, 4, 5, 6]
-    elif num == 7:
-        index = [0, 2, 5]
-    elif num == 8:
-        index = [0, 1, 2, 3, 4, 5, 6]
-    elif num == 9:
-        index = [0, 1, 2, 3, 5, 6]
-    return index
+        # Personalized exampt rules
+        # Format: [weekday, start_hour, start_min, end_hour, end_min]
+        self.rule_exempt_period = [[3, 13, 0, 14, 30], [4, 0, 0, 15, 0]]
 
-def lineindex_to_line(index):
-    xlist = []
-    ylist = []
+        self.register_turning_point()
+        self.register_exempt_time()
 
-    if index == 0:
-        xlist = [0+gap, 1-gap]
-        ylist = [2, 2]
-    elif index == 1:
-        xlist = [0, 0]
-        ylist = [1+gap, 2-gap]
-    elif index == 2:
-        xlist = [1, 1]
-        ylist = [1+gap, 2-gap]
-    elif index == 3:
-        xlist = [0+gap, 1-gap]
-        ylist = [1, 1]
-    elif index == 4:
-        xlist = [0, 0]
-        ylist = [0+gap, 1-gap]
-    elif index == 5:
-        xlist = [1, 1]
-        ylist = [0+gap, 1-gap]
-    elif index == 6:
-        xlist = [0+gap, 1-gap]
-        ylist = [0, 0]
-    return xlist, ylist
+    # map num 0-9 to index in digital display
+    def num_to_lineindex(self, num):
+        index = []
+        if num == 0:
+            index = [0, 1, 2, 4, 5, 6]
+        elif num == 1:
+            index = [2, 5]
+        elif num == 2:
+            index = [0, 2, 3, 4, 6]
+        elif num == 3:
+            index = [0, 2, 3, 5, 6]
+        elif num == 4:
+            index = [1, 2, 3, 5]
+        elif num == 5:
+            index = [0, 1, 3, 5, 6]
+        elif num == 6:
+            index = [0, 1, 3, 4, 5, 6]
+        elif num == 7:
+            index = [0, 2, 5]
+        elif num == 8:
+            index = [0, 1, 2, 3, 4, 5, 6]
+        elif num == 9:
+            index = [0, 1, 2, 3, 5, 6]
+        return index
 
-# num = 0-9, digit = 0-3 (set position)
-def num_to_digit(ax, num, digit):
-    for each_index in num_to_lineindex(num):
-        xlist, ylist = lineindex_to_line(each_index)
-        ax.plot(np.array(xlist)+base_pos[digit][0], np.array(ylist)+base_pos[digit][1], 'k', linewidth = linewidth)
+    def lineindex_to_line(self, index):
+        gap = self.gap
+        xlist = []
+        ylist = []
 
-# hour = 0-23, minute = 00-59
-def plot_time(ax, hour, minute):
-    # Plot four digit
-    num_to_digit(ax, int(hour/10), 0)
-    num_to_digit(ax, int(hour%10), 1)
-    num_to_digit(ax, int(minute/10), 2)
-    num_to_digit(ax, int(minute%10), 3)
+        if index == 0:
+            xlist = [0+gap, 1-gap]
+            ylist = [2, 2]
+        elif index == 1:
+            xlist = [0, 0]
+            ylist = [1+gap, 2-gap]
+        elif index == 2:
+            xlist = [1, 1]
+            ylist = [1+gap, 2-gap]
+        elif index == 3:
+            xlist = [0+gap, 1-gap]
+            ylist = [1, 1]
+        elif index == 4:
+            xlist = [0, 0]
+            ylist = [0+gap, 1-gap]
+        elif index == 5:
+            xlist = [1, 1]
+            ylist = [0+gap, 1-gap]
+        elif index == 6:
+            xlist = [0+gap, 1-gap]
+            ylist = [0, 0]
+        return xlist, ylist
 
-    # Two points in the middle of clock
-    ax.plot(4, 1.5, 'ok', markersize=linewidth)
-    ax.plot(4, 2.5, 'ok', markersize=linewidth)
+    # num = 0-9, digit = 0-3 (set position)
+    def num_to_digit(self, ax, num, digit):
+        for each_index in self.num_to_lineindex(num):
+            xlist, ylist = self.lineindex_to_line(each_index)
+            ax.plot(np.array(xlist) + self.base_pos[digit][0], np.array(ylist) + self.base_pos[digit][1], 'k', linewidth = self.linewidth)
 
-def ax_standard(ax):
-    # Plot a square as edge
-    xlim = [0, 8]
-    ylim = [0, 4]
-    kwarg = {'linewidth': 5, 'color': 'grey', 'alpha': 1}
-    ax.plot(xlim, [ylim[0], ylim[0]], **kwarg)
-    ax.plot(xlim, [ylim[1], ylim[1]], **kwarg)
-    ax.plot([xlim[0], xlim[0]], ylim, **kwarg)
-    ax.plot([xlim[1], xlim[1]], ylim, **kwarg)
+    # hour = 0-23, minute = 00-59
+    def plot_time(self, ax, hour, minute, task_name = ""):
+        # Plot four digit
+        self.num_to_digit(ax, int(hour/10), 0)
+        self.num_to_digit(ax, int(hour%10), 1)
+        self.num_to_digit(ax, int(minute/10), 2)
+        self.num_to_digit(ax, int(minute%10), 3)
 
-    ax.set_facecolor('lightgrey')
-    ax.axis('equal')
-    ax.axis("off")
+        # Two points in the middle of clock
+        ax.plot(4, 1.5, 'ok', markersize = self.linewidth)
+        ax.plot(4, 2.5, 'ok', markersize = self.linewidth)
 
-def hour_to_minute(hour, minute):
-    return 60*hour+minute
+        # Write the task name
+        ax.text(4, 0, task_name, fontsize = 15, horizontalalignment = "center", verticalalignment = "center")
 
-def minute_to_hour(minute):
-    return int(int(minute/60)%24), int(minute%60)
+    def ax_standard(self, ax):
+        # Plot a square as edge
+        xlim = [0, 8]
+        ylim = [0, 4]
+        kwarg = {'linewidth': 5, 'color': 'grey', 'alpha': 1}
+        ax.plot(xlim, [ylim[0], ylim[0]], **kwarg)
+        ax.plot(xlim, [ylim[1], ylim[1]], **kwarg)
+        ax.plot([xlim[0], xlim[0]], ylim, **kwarg)
+        ax.plot([xlim[1], xlim[1]], ylim, **kwarg)
 
-def time_transform(hour, minute):
-    time_cal = hour_to_minute(hour, minute)
-    time_new = 0
+        ax.set_facecolor('lightgrey')
+        ax.axis('equal')
+        ax.axis("off")
 
-    hour_new = 0
-    minute_new = 0
+    def hour_to_minute(self, hour, minute):
+        return 60*hour+minute
 
-    if 0 <= hour < 7:
-        time_new = time_cal + hour_to_minute(5,30)
-    elif 7 <= hour < 8:
-        time_new = (time_cal - hour_to_minute(7,0)) * 2.5 + hour_to_minute(12,30)
-    elif 8 <= hour < 11:
-        time_new = (time_cal - hour_to_minute(8,0)) * 2/3 + hour_to_minute(2,30)
-    elif 11 <= hour < 12:
-        time_new = (time_cal - hour_to_minute(11,0)) * 2.5 + hour_to_minute(12,30)
-    elif 12 <= hour < 16:
-        time_new = (time_cal - hour_to_minute(12,0)) / 2 + hour_to_minute(2,30)
-    elif hour_to_minute(16,0) <= time_cal < hour_to_minute(17,30):
-        time_new = (time_cal - hour_to_minute(16,0)) * 4/3 + hour_to_minute(18,0)
-    elif hour_to_minute(17,30) <= time_cal < hour_to_minute(19,0):
-        time_new = time_cal - hour_to_minute(17,30) + hour_to_minute(20,0)
-    elif 19 <= hour < 20:
-        time_new = (time_cal - hour_to_minute(19,0)) * 3 + hour_to_minute(21,30)
-    elif hour_to_minute(20,0) <= time_cal < hour_to_minute(22,30):
-        time_new = (time_cal - hour_to_minute(20,0)) * 1.8 + hour_to_minute(0,30)
-    elif hour_to_minute(22,30) <= time_cal < hour_to_minute(24,00):
-        time_new = (time_cal - hour_to_minute(22,30)) / 3 + hour_to_minute(5,0)
+    def minute_to_hour(self, minute):
+        return int(int(minute/60)%24), int(minute%60)
 
-    hour_new, minute_new = minute_to_hour(time_new)
-    return hour_new, minute_new
+    def time_transform(self, hour, minute):
+        time_cal = self.hour_to_minute(hour, minute)
+        time_new = 0
 
-time_rule_turning_point = []
-time_rule_turning_point.append(hour_to_minute(0,0))
-time_rule_turning_point.append(hour_to_minute(7,0))
-time_rule_turning_point.append(hour_to_minute(8,0))
-time_rule_turning_point.append(hour_to_minute(11,0))
-time_rule_turning_point.append(hour_to_minute(12,0))
-time_rule_turning_point.append(hour_to_minute(16,0))
-time_rule_turning_point.append(hour_to_minute(17,30))
-time_rule_turning_point.append(hour_to_minute(19,0))
-time_rule_turning_point.append(hour_to_minute(20,0))
-time_rule_turning_point.append(hour_to_minute(22,30))
+        hour_new = 0
+        minute_new = 0
+        task_name = ""
 
-# print(time.localtime(time.time()))
-# tm_mon=7, tm_mday=12
-# tm_hour=1, tm_min=29
-# tm_wday=6
+        for rule_idx in range(len(self.real_turning_point) - 1):
+            if self.real_turning_point[rule_idx] <= time_cal < self.real_turning_point[rule_idx + 1]:
+                time_new = self.rule_speed[rule_idx] * (time_cal - self.real_turning_point[rule_idx]) + self.ref_turning_point[rule_idx]
+                task_name = self.rule_name[rule_idx] + "，或者不" + self.rule_name[rule_idx] + "，是你的选择"
+                break
 
-# Main function begins here
-fig, ax = plt.subplots(figsize=(4,2))
-fig.canvas.set_window_title("Time Turner")
-while True:
-    plt.cla()
+        hour_new, minute_new = self.minute_to_hour(time_new)
+        return hour_new, minute_new, task_name
 
-    hour = time.localtime(time.time()).tm_hour
-    minute = time.localtime(time.time()).tm_min
+    def register_turning_point(self):
+        self.real_turning_point = []
+        for each in self.rule_turning_point:
+            self.real_turning_point.append(self.hour_to_minute(each[0], each[1]))
 
-    hour_new = 0
-    minute_new = 0
+        self.ref_turning_point = []
+        for each in self.rule_ref_turning_point:
+            self.ref_turning_point.append(self.hour_to_minute(each[0], each[1]))
 
-    if time.localtime(time.time()).tm_wday == 4 and 0 <= hour_to_minute(hour, minute) < hour_to_minute(13,0):
-        hour_new = hour
-        minute_new = minute
-    else:
-        hour_new, minute_new = time_transform(hour, minute)
+    def register_exempt_time(self):
+        self.exempt_time = []
+        # exempt_time format = [weekday, start time (minute), end time (minute)]
+        for each in self.rule_exempt_period:
+            self.exempt_time.append([each[0], self.hour_to_minute(each[1], each[2]), self.hour_to_minute(each[3], each[4])])
 
-    plot_time(ax, hour_new, minute_new)
-    if hour_to_minute(hour, minute) in time_rule_turning_point:
-        [winsound.Beep(440, 200) for _ in range(4)]
-    
-    ax_standard(ax)
-    fig.tight_layout()
+    # print(time.localtime(time.time()))
+    # tm_mon=7, tm_mday=12
+    # tm_hour=1, tm_min=29
+    # tm_wday=6
 
-    plt.pause(20)
-plt.ioff()
+    # Main function begins here
+    def main(self):
+        fig, ax = plt.subplots(1, 1, figsize=(4,2))
+        fig.canvas.set_window_title("Time Turner")
+        
+        mngr = plt.get_current_fig_manager()
+        if get_backend() == "Qt5Agg":
+            fig.canvas.manager.window.move(625, 0)
+            fig.canvas.toolbar.setVisible(False)
+        elif get_backend() == "TkAgg":
+            fig.canvas.manager.window.wm_geometry("+880+0")
+            fig.canvas.toolbar.pack_forget()
 
+        fig.canvas.mpl_disconnect(fig.canvas.manager.key_press_handler_id)
 
+        while True:
+            plt.cla()
+
+            hour = time.localtime(time.time()).tm_hour
+            minute = time.localtime(time.time()).tm_min
+
+            hour_new = 0
+            minute_new = 0
+            task_name = ""
+
+            exempt_flag = False
+            for each_exempt in self.exempt_time:
+                # tm_wday = weekday - 1 (because start from 0)
+                if time.localtime(time.time()).tm_wday+1 == each_exempt[0] and each_exempt[1] <= self.hour_to_minute(hour, minute) < each_exempt[2]:
+                    hour_new = hour
+                    minute_new = minute
+                    exempt_flag = True
+                    break
+            if exempt_flag == False:
+                hour_new, minute_new, task_name = self.time_transform(hour, minute)
+
+            self.plot_time(ax, hour_new, minute_new, task_name)
+            if minute == 0:
+                winsound.Beep(440, 200)
+            if self.hour_to_minute(hour, minute) in self.real_turning_point:
+                if minute == 0:
+                    [winsound.Beep(440, 200) for _ in range(3)]
+                else:
+                    [winsound.Beep(440, 200) for _ in range(4)]
+
+            self.ax_standard(ax)
+            
+            fig.tight_layout()
+            fig.set_size_inches((4, 2))
+
+            plt.pause(20)
+        plt.ioff()
+
+plt.rcParams['font.sans-serif'] = ['SimHei']
+
+clock = Clock()
+if __name__ == "__main__":
+    clock.main()
